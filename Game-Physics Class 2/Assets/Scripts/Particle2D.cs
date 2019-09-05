c9b2f51cb30463fc87a102772489517211e2d348
John Connelly <john.connelly@mymail.champlain.edu>
2019-09-05 17:58:03 -0400

    commit 1

---
+++ Game-Physics Class 2/Assets/Scripts/Particle2D.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle2D : MonoBehaviour
+{
+    public Vector2 position;
+    public Vector2 velocity;
+    public Vector2 acceleration;
+
+
+    public float rotation;
+    public float angularVelocity;
+    public float angularAcceleration;
+    public dropDownPhysics phystype;
+    public Oscillation circtype;
+
+    public enum dropDownPhysics
+    {
+        Kinematic,
+        Euler
+    }
+
+    public enum Oscillation
+    {
+        Oscillate,
+        None
+    }
+
+    void updatePositionEulerExplicit(float deltaTime)
+    {
+        position += velocity * deltaTime;
+
+        velocity += acceleration * deltaTime;
+    }
+
+    void updatePositionKinematic(float deltaTime)
+    {
+        position += velocity * deltaTime + acceleration * deltaTime * deltaTime * 0.5f;
+
+        velocity += acceleration * deltaTime;
+    }
+
+    void updateRotationEulerExplicit(float deltaTime)
+    {
+        rotation += angularVelocity * deltaTime;
+
+        angularVelocity += angularAcceleration * deltaTime;
+    }
+
+    void updateRotationKinematic(float deltaTime)
+    {
+        rotation += angularVelocity * deltaTime + angularAcceleration * deltaTime * deltaTime * 0.5f;
+
+        angularVelocity += angularAcceleration * deltaTime;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (phystype == dropDownPhysics.Euler)
+        {
+            updatePositionEulerExplicit(Time.fixedDeltaTime);
+
+            updateRotationEulerExplicit(Time.fixedDeltaTime);
+        }
+
+        if (phystype == dropDownPhysics.Kinematic)
+        {
+            updatePositionKinematic(Time.fixedDeltaTime);
+
+            updateRotationKinematic(Time.fixedDeltaTime);
+        }
+
+        if(circtype == Oscillation.Oscillate)
+        {
+            //Save time when object instantiated
+            //subtract that time from current time
+            //do sin of created time onto x and y pos
+
+        }
+
+        transform.position = position;
+
+        transform.eulerAngles = new Vector3(0f,0f,rotation);
+
+        acceleration.x = -Mathf.Sin(Time.time);
+    }
+}
