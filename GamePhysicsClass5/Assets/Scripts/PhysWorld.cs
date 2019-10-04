85384874632b4240e45d730f55ba1eb564ad187f
John Connelly <john.connelly@mymail.champlain.edu>
2019-10-03 20:52:22 -0400

    asdoasdhahsd

--- GamePhysicsClass5/Assets/Scripts/PhysWorld.cs
+++ GamePhysicsClass5/Assets/Scripts/PhysWorld.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class PhysWorld : MonoBehaviour
 {
-    public static int objectAmount = 3;
-    public GameObject[] physObjects = new GameObject[objectAmount];
-    public GameObject test1;
-    public GameObject test2;
-    public GameObject test3;
-    public GameObject test4;
-    public GameObject test5;
-    public GameObject test6;
+    //public static int objectAmount = 3;
+    //public GameObject[] physObjects = new GameObject[objectAmount];
+    public GameObject circle1;
+    public GameObject circle2;
+    public GameObject aabb1;
+    public GameObject aabb2;
+    public GameObject obb1;
+    public GameObject obb2;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,46 +21,97 @@ public class PhysWorld : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //CheckCollisions();
-        if (CollisionHull2D.AABBAABB(test3.GetComponent<AxisAllignedBoundingBoxCollision2D>(), test4.GetComponent<AxisAllignedBoundingBoxCollision2D>()))
-        {
-            Debug.Log("Collision AABB AABB");
-        }
-        if (CollisionHull2D.CircleCircle(test1.GetComponent<CircleCollision2D>(), test2.GetComponent<CircleCollision2D>()))
-        {
-            Debug.Log("Collision Circ Circ");
-        }
-        if (CollisionHull2D.CircleAABB(test1.GetComponent<CircleCollision2D>(), test3.GetComponent<AxisAllignedBoundingBoxCollision2D>()))
-        {
-            Debug.Log("Collision Circ AABB");
-
-
-        }
-        if(CollisionHull2D.AABBOBB(test3.GetComponent<AxisAllignedBoundingBoxCollision2D>(), test5.GetComponent<ObjectBoundingBoxCollision2D>()))
-        {
-            Debug.Log("Collision AABB OBB");
-
-        }
-        if (CollisionHull2D.CircleOBB(test2.GetComponent<CircleCollision2D>(), test6.GetComponent<ObjectBoundingBoxCollision2D>()))
-        {
-            Debug.Log("Collision Circ OBB");
-        }
-        if (CollisionHull2D.OBBOBB(test5.GetComponent<ObjectBoundingBoxCollision2D>(), test6.GetComponent<ObjectBoundingBoxCollision2D>()))
-        {
-            Debug.Log("Collision OBB OBB");
-
-        }
+        CheckCollisionsDebug();
+        
 
 
     }
 
+
+    void CheckCollisionsDebug()
+    {
+        if (CollisionHull2D.AABBAABB(aabb1.GetComponent<AxisAllignedBoundingBoxCollision2D>(), aabb2.GetComponent<AxisAllignedBoundingBoxCollision2D>()))
+        {
+            Debug.Log("Collision AABB AABB");
+        }
+
+        if (CollisionHull2D.CircleCircle(circle1.GetComponent<CircleCollision2D>(), circle2.GetComponent<CircleCollision2D>()))
+        {
+            Debug.Log("Collision Circ Circ");
+        }
+        if (CollisionHull2D.CircleAABB(circle1.GetComponent<CircleCollision2D>(), aabb1.GetComponent<AxisAllignedBoundingBoxCollision2D>()))
+        {
+            Debug.Log("Collision Circ1 AABB1");
+
+
+        }
+        if (CollisionHull2D.CircleAABB(circle2.GetComponent<CircleCollision2D>(), aabb1.GetComponent<AxisAllignedBoundingBoxCollision2D>()))
+        {
+            Debug.Log("Collision Circ2 AABB1");
+
+
+        }
+        if (CollisionHull2D.CircleAABB(circle2.GetComponent<CircleCollision2D>(), aabb2.GetComponent<AxisAllignedBoundingBoxCollision2D>()))
+        {
+            Debug.Log("Collision Circ2 AABB2");
+
+
+        }
+        if (CollisionHull2D.CircleAABB(circle1.GetComponent<CircleCollision2D>(), aabb2.GetComponent<AxisAllignedBoundingBoxCollision2D>()))
+        {
+            Debug.Log("Collision Circ1 AABB2");
+
+
+        }
+        if (CollisionHull2D.AABBOBB(aabb1.GetComponent<AxisAllignedBoundingBoxCollision2D>(), obb1.GetComponent<ObjectBoundingBoxCollision2D>()))
+        {
+            Debug.Log("Collision AABB1 OBB1");
+
+        }
+        if (CollisionHull2D.AABBOBB(aabb1.GetComponent<AxisAllignedBoundingBoxCollision2D>(), obb2.GetComponent<ObjectBoundingBoxCollision2D>()))
+        {
+            Debug.Log("Collision AABB1 OBB2");
+
+        }
+        if (CollisionHull2D.AABBOBB(aabb2.GetComponent<AxisAllignedBoundingBoxCollision2D>(), obb1.GetComponent<ObjectBoundingBoxCollision2D>()))
+        {
+            Debug.Log("Collision AABB2 OBB1");
+
+        }
+        if (CollisionHull2D.AABBOBB(aabb2.GetComponent<AxisAllignedBoundingBoxCollision2D>(), obb2.GetComponent<ObjectBoundingBoxCollision2D>()))
+        {
+            Debug.Log("Collision AABB2 OBB2");
+
+        }
+        if (CollisionHull2D.CircleOBB(circle2.GetComponent<CircleCollision2D>(), obb2.GetComponent<ObjectBoundingBoxCollision2D>()))
+        {
+            Debug.Log("Collision Circ2 OBB2");
+        }
+        if (CollisionHull2D.CircleOBB(circle1.GetComponent<CircleCollision2D>(), obb1.GetComponent<ObjectBoundingBoxCollision2D>()))
+        {
+            Debug.Log("Collision Circ1 OBB1");
+        }
+        if (CollisionHull2D.CircleOBB(circle1.GetComponent<CircleCollision2D>(), obb2.GetComponent<ObjectBoundingBoxCollision2D>()))
+        {
+            Debug.Log("Collision Circ1 OBB2");
+        }
+        if (CollisionHull2D.CircleOBB(circle2.GetComponent<CircleCollision2D>(), obb1.GetComponent<ObjectBoundingBoxCollision2D>()))
+        {
+            Debug.Log("Collision Circ2 OBB1");
+        }
+        if (CollisionHull2D.OBBOBB(obb1.GetComponent<ObjectBoundingBoxCollision2D>(), obb2.GetComponent<ObjectBoundingBoxCollision2D>()))
+        {
+            Debug.Log("Collision OBB OBB");
+
+        }
+    }
     /*
      * create an array of all objects in the world, and create a nested loop that loops through each one with the other
      * comparing if the item collides with another based on the enum type of the other to decide the  collision
      * 
      * 
      */
-    void CheckCollisions()
+    /*void CheckCollisions()
      {
         int currentShape = 0;
         for(int i = currentShape; i < objectAmount; i++)
@@ -154,6 +205,6 @@ public class PhysWorld : MonoBehaviour
                 }
             }
         }
-     }
+     }*/
 
 }
